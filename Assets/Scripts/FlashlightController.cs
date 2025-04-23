99e1e85edc04e4993e09a473c5e759d9242989ef
chillwafflez <ju5t1n.nguy3n284@gmail.com>
2025-04-23 05:29:18 -0700

    added dark atmosphere and basic flashlight functionality

---
+++ Assets/Scripts/FlashlightController.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+
+public class FlashlightController : MonoBehaviour
+{
+    public Light flashlight;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            flashlight.enabled = !flashlight.enabled;
+            Console.Write("flashlight toggled");
+        }
+    }
+}
