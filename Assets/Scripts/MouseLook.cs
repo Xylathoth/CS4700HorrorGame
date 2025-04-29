af272a199542f8e26e52e33a13ec96cb3f68e70e
Xylathoth <bill9192003@gmail.com>
2025-04-28 23:49:41 -0700

    Revert "Merge branch 'main' into NathanBranch"
    This reverts commit 9b1ab206a600c841f97b47bbf2561bc705b3263b, reversing
    changes made to 0c748b4ffce90795552e222fb4d746c975c2f7d4.

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -1,46 +1,18 @@
-using System;
 using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
     public Transform playerBody;
     public float mouseSensitivity = 100f;
-    public Boolean isInverted;
-    float mouseX;
-    float mouseY;
 
     float xRotation = 0f;
 
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
-        transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
-        isInverted = false;
     }
 
     void Update()
-    {
-        if (isInverted)
-        {
-            InvertedMovement();
-        } else
-        {
-            NormalMovement();
-        }
-
-        
-
-        //float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        //float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
-
-        //xRotation -= mouseY;
-        //xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-        //transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-
-        //playerBody.Rotate(Vector3.up * mouseX);
-    }
-
-    void NormalMovement()
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
@@ -51,18 +23,4 @@ public class MouseLook : MonoBehaviour
 
         playerBody.Rotate(Vector3.up * mouseX);
     }
-
-    void InvertedMovement()
-    {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
-
-        xRotation += mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-
-        playerBody.Rotate(Vector3.down * mouseX);
-    }
-
-
 }
